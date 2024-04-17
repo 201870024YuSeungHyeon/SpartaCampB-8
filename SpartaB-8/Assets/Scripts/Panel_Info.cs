4c483ebed6efc67ba130d16ab27e7be2f5195890
201870024YuSeungHyeon <jyyootom@naver.com>
2024-04-17 16:47:15 +0900

    최고점수 불러와지지 않는 버그 수정

--- SpartaB-8/Assets/Scripts/Panel_Info.cs
+++ SpartaB-8/Assets/Scripts/Panel_Info.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.SocialPlatforms.Impl;
 
 public class Panel_Info : MonoBehaviour
 {
@@ -18,11 +18,14 @@ public class Panel_Info : MonoBehaviour
 
     public void Show(string name)
     {
+
         sceneName = name;
 
         if (!PlayerPrefs.HasKey(name))
         {
+            
             _score = PlayerPrefs.GetInt(name);
+            Debug.Log($"이름 : {name} , 점수 : {_score}");
             TxtHighScore.text = "BEST\n" + _score;
         }
         else
@@ -37,6 +40,8 @@ public class Panel_Info : MonoBehaviour
     public void OnClick_Play()
     {
         SceneManager.LoadScene(sceneName);
+        DataManager.Instance.key = sceneName;
+
     }
 
     public void OnClick_GoMain()
